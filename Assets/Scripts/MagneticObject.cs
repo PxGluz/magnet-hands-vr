4a14191b341878c350c5f740b35b00db2111cb9f
Popa Bogdan-Gabriel <bogdan.popa.g25@gmail.com>
2025-12-07 18:30:53 +0200

    pullable checkpoints + fixes

--- Assets/Scripts/MagneticObject.cs
+++ Assets/Scripts/MagneticObject.cs
@@ -2,6 +2,27 @@ using UnityEngine;
 
 public class MagneticObject : MonoBehaviour
 {
+    [SerializeField] private Material positiveColor;
+    [SerializeField] private Material negativeColor;
+
     public bool isPullable = false;
     public MagneticInputLogic.MagnetismType magneticPole = MagneticInputLogic.MagnetismType.Negative;
+
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    public void RespawnItem()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        transform.SetPositionAndRotation(initialPosition, initialRotation);
+    }
+
+    void Start()
+    {
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+
+        GetComponent<MeshRenderer>().material = magneticPole == MagneticInputLogic.MagnetismType.Negative ? negativeColor : positiveColor;
+    }
 }
